db31bad32e83bc5c1bc4ebfec930a330dcacc39b
Berk <connect@berkacar.dev>
2024-03-25 10:45:17 +0300

    UI

--- player.gd
+++ player.gd
@@ -1,83 +1,74 @@
 extends CharacterBody3D
 
-# Emitted when the player was hit by a mob
-# Put this at the top of the script.
 signal hit
 
-# How fast the player moves in meters per second.
+## How fast the player moves in meters per second.
 @export var speed = 14
-# The downward acceleration when in the air, in meters per second squared.
-@export var fall_acceleration = 75
-# Vertical impulse applied to the character upon jumping in meters per second.
+## Vertical impulse applied to the character upon jumping in meters per second.
 @export var jump_impulse = 20
-# Vertical impulse applied to the character upon bouncing over a mob in meters per second.
+## Vertical impulse applied to the character upon bouncing over a mob in meters per second.
 @export var bounce_impulse = 16
+## The downward acceleration when in the air, in meters per second.
+@export var fall_acceleration = 75
 
-
-var target_velocity = Vector3.ZERO
 
 func _physics_process(delta):
-	# We create a local variable to store the input direction.
 	var direction = Vector3.ZERO
-	
-	# We check for each move input and update the direction accordingly.
 	if Input.is_action_pressed("move_right"):
 		direction.x += 1
 	if Input.is_action_pressed("move_left"):
 		direction.x -= 1
 	if Input.is_action_pressed("move_back"):
-		# Notice how we re working with the vector's x and z axes.
-		# In 3D, the XZ plane is the ground plane.
 		direction.z += 1
 	if Input.is_action_pressed("move_forward"):
 		direction.z -= 1
-		
+
 	if direction != Vector3.ZERO:
+		# In the lines below, we turn the character when moving and make the animation play faster.
 		direction = direction.normalized()
 		# Setting the basis property will affect the rotation of the node.
-		$Pivot.basis = Basis.looking_at(direction)
-		
-	# Ground Velocity
-	target_velocity.x = direction.x * speed
-	target_velocity.z = direction.z * speed
-	
-	# Vertical Velocity
-	if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
-		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
+		basis = Basis.looking_at(direction)
+		$AnimationPlayer.speed_scale = 4
+	else:
+		$AnimationPlayer.speed_scale = 1
 
-	# Moving the Character
-	velocity = target_velocity
-	
-	# Jumping
+	velocity.x = direction.x * speed
+	velocity.z = direction.z * speed
+
+	# Jumping.
 	if is_on_floor() and Input.is_action_just_pressed("jump"):
-		target_velocity.y = jump_impulse
-		
-	# Iterate through all collisions that occurrred this frame	
+		velocity.y += jump_impulse
+
+	# We apply gravity every frame so the character always collides with the ground when moving.
+	# This is necessary for the is_on_floor() function to work as a body can always detect
+	# the floor, walls, etc. when a collision happens the same frame.
+	velocity.y -= fall_acceleration * delta
+	move_and_slide()
+
+	# Here, we check if we landed on top of a mob and if so, we kill it and bounce.
+	# With move_and_slide(), Godot makes the body move sometimes multiple times in a row to
+	# smooth out the character's motion. So we have to loop over all collisions that may have
+	# happened.
+	# If there are no "slides" this frame, the loop below won't run.
 	for index in range(get_slide_collision_count()):
-		# We get one of the collisions with the player
 		var collision = get_slide_collision(index)
-		
-		# If the collision is with ground
-		if collision.get_collider() == null:
-			continue
-		
-		# If the collider is with a mob
 		if collision.get_collider().is_in_group("mob"):
 			var mob = collision.get_collider()
-			# We check that we re hitting it from above
 			if Vector3.UP.dot(collision.get_normal()) > 0.1:
-				# If so, we squash it and bounce.
 				mob.squash()
-				target_velocity.y = bounce_impulse
-				# Prevent further dublicate calls.
+				velocity.y = bounce_impulse
+				# Prevent this block from running more than once,
+				# which would award the player more than 1 point for squashing a single mob.
 				break
-				
-	
-	move_and_slide()
+
+	# This makes the character follow a nice arc when jumping
+	rotation.x = PI / 6 * velocity.y / jump_impulse
+
 
 func die():
 	hit.emit()
 	queue_free()
 
-func _on_mob_detector_body_entered(_body):
+
+func _on_MobDetector_body_entered(_body):
 	die()
